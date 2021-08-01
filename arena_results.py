269d32ba23fd37f62ad036056ea48200246d5d7a
liamdj <liamdj@princeton.edu>
2021-08-01 01:26:49 -0400

    Initial commit

---
+++ arena_results.py
@@ -0,0 +1,68 @@
+import bga_scraping as bga
+import json
+from collections import namedtuple
+import pandas as pd
+
+SEASONS = {
+    4: {'start': 1609822800, 'end': 1617681600},
+    5: {'start': 1617681600, 'end': 1625544000}
+}
+
+#    bga.save_table_replays_batch(bga.session_generator(), tables.keys(), path)
+
+def parse_game_result(file_path):
+    """ Returns map from player id to traveler, position, score, and rank for each player from a stored table replay,
+        if the file exists.
+    """
+
+    outcomes = {}
+    try:
+        with open(file_path) as file:
+            replay = json.load(file)
+            start_infos = {}
+            for move in replay:
+                if not start_infos:
+                    for data in move['data']:
+                        if data['type'] == 'travelerChosen':
+                            start_infos[data['args']['player_id']] = data['args']
+
+            last_move = next(move['data'] for move in reversed(replay) if move["move_id"] != None)
+            results = last_move[len(last_move) - 2]['args']['args']['result']
+            ranks = [res['rank'] for res in results]
+
+            for index in range(len(results)):
+                placement = (sum(1 if ranks[index] < r else 0.5 if ranks[index] == r else 0 for r in ranks) - 0.5)
+                score = int(results[index]['score'])
+                achievements = int(results[index]['score_aux'])
+                id = int(results[index]['player'])
+                outcomes[id] = start_infos[id]['traveler'], int(start_infos[id]['player_position_order']), placement, score, achievements
+
+            return outcomes
+
+    except FileNotFoundError:
+        print('File {} does not exist.'.format(file_path))
+
+def summary_data(df):
+    return len(df.index), df['placement'].mean(), df['placement'].std(), df['points'].mean(), df['points'].std(), df['achievements'].mean()
+
+def create_results_summary(tables):
+
+    Result = namedtuple('Result', ['table_id', 'traveler', 'position', 'placement', 'points', 'achievements'])
+    results = []
+    for table_id, players in tables.items():
+        outcomes = parse_game_result('s5-arena/replays/{}.json'.format(table_id))
+        if outcomes:
+            results.extend([Result(table_id, res[0], res[1], res[2], res[3], res[4]) for id, res in outcomes.items() if id in players])
+    data = pd.DataFrame(results)
+    data.to_csv('s5-arena/results.csv')
+
+    rows = {}
+    rows['all'] = summary_data(data)
+    for trvl in data["traveler"].unique():
+        rows[trvl] = summary_data(data.loc[data['traveler'] == trvl])
+    for pos in range(1, 5):
+        rows[str(pos)] = summary_data(data.loc[data['position'] == pos])
+    
+    summary = pd.DataFrame.from_dict(rows, orient='index', columns=['count', 'placement avg', 'placement std', 'points avg', 'points std', 'achievements avg'])
+    summary.insert(0, 'frequency', summary['count'] / len(data))
+    summary.to_csv("s5-arena/summary.csv")
---
+++ bga_scraping.py
@@ -0,0 +1,107 @@
+import requests
+from accounts import ACCOUNTS
+import json
+from bs4 import BeautifulSoup
+import os.path
+from collections import defaultdict
+
+BASE = 'https://boardgamearena.com'
+LOGIN = '/account/account/login.html'
+RANKING = '/halloffame/halloffame/getRanking.html'
+GAMES = '/gamestats/gamestats/getGames.html'
+ARCHIVE = '/gamereview/gamereview/requestTableArchive.html'
+REPLAY = '/archive/archive/logs.html'
+GAME_ID = 1003
+
+DEPELETED = 'You have reached a limit (replay)'
+NO_ACCESS = 'Sorry, you need to be registered more than 24 hours and have played at least 2 games to access this feature.'
+
+def session_generator():
+    # must be a verified bga account with >=2 games and >24 hours old
+    for email, password in ACCOUNTS:
+        sess = requests.Session()
+        # request to a login page needed to produce csrf token
+        resp = sess.get(BASE + '/account')
+        soup = BeautifulSoup(resp.content, 'html.parser')
+        csrf_token = soup.find(id='csrf_token')['value']
+        login_info = {'email': email, 'password': password, 'rememberme': 'off', 'redirect': 'join', 'form_id': 'loginform', 'csrf_token': csrf_token}
+        sess.post(BASE + LOGIN, data=login_info)
+        yield sess
+
+def get_top_tables_players(sess, season, start_date, end_date, number=10):
+    """ Returns a map with table ids to player ids for arena games with the top players.
+    """
+
+    tables = defaultdict(list)
+    for rank in range(1, number + 1):
+        player_id = get_player_by_rank(sess, rank, season)
+        for table_id in get_arena_tables(sess, player_id, start_date, end_date):
+            tables[table_id].append(player_id)
+    return tables
+
+def get_player_by_rank(sess, rank, season):
+    """ Returns the id for the player with given rank for given arena season.
+    """
+
+    params = {'start': rank-1, 'game': GAME_ID, 'mode': 'arena', 'season': season}
+    resp = sess.get(BASE + RANKING, params=params)
+    return int(resp.json()['data']['ranks'][0]['id'])
+
+def get_arena_tables(sess, player_id, start_date, end_date):
+    """ Returns the table ids of ranked arena games played between dates for player.
+    """
+
+    params = {'page': 0, 'player': player_id, 'game_id': GAME_ID, 'start_date': start_date, 'end_date': end_date, 'finished': 1, 'updateStats': 0}
+    table_ids = []
+    while True:
+        params['page'] += 1
+        resp = sess.get(BASE + GAMES, params=params)
+        ids = [int(table['table_id']) for table in resp.json()['data']['tables'] if table['arena_win'] != None and table['unranked'] != '1']
+        if len(ids) == 0:
+            break
+        table_ids.extend(ids)
+    return table_ids
+
+def save_table_replays_batch(table_ids, path):
+    """ Saves replay logs for a list of tables. 
+        Once an account reaches its replay limit, uses the next session from the generator. 
+        Ignores table_ids for which a file already exists.
+    """
+
+    generator = session_generator()
+    sess = next(generator, None)
+    for index, id in enumerate(table_ids):
+        if not os.path.isfile('{}/{}.json'.format(path, id)):
+            while sess and save_table_replay(sess, id, path):
+                sess = next(generator, None)
+        if not sess:
+            print("Replays depleted for all logins. There are {} table replays remaining.".format(len(table_ids) - index))
+            break
+    return sess
+
+def save_table_replay(sess, table_id, path):
+    """ Saves replay log as json to file.
+        Returns true if the current account is unable to access further replays.
+    """
+
+    # seemingly required to produce log
+    sess.get(BASE + ARCHIVE, params={'table': table_id})
+    resp = sess.get(BASE + REPLAY, params={'table': table_id, 'translated': 'true'})
+    j = resp.json()
+    if 'error' in j:
+        if j['error'] == DEPELETED:
+            print('Account replay access depleted.')
+            return True
+        elif j['error'] == NO_ACCESS:
+            print('Account cannot access any replays.')
+            return True
+    
+    try:
+        moves = j['data']['data']['data']
+        with open('{}/{}.json'.format(path, table_id), 'w', encoding='utf-8') as file:
+            json.dump(moves, file, ensure_ascii=False)
+    except:
+        print("Error saving replay for table {}:".format(table_id))
+        print(resp.text)
+    return False
+
